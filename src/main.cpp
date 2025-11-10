19a52bb624d965806cccfebc74ddbceb05407027
demonkiller <gaurangvishwakarma619@gmail.com>
2025-11-10 08:31:05 +0000

    initialise qt6 app

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include "gui/mainwindow.h"
+#include "utils/logger.h"
+#include <QApplication>
+#include <QStyleFactory>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    
+    // Set application metadata
+    app.setApplicationName("ALG App Store");
+    app.setApplicationVersion("2.0.0");
+    app.setOrganizationName("Arch Linux GUI");
+    
+    // Set application style
+    app.setStyle(QStyleFactory::create("Fusion"));
+    
+    Logger::info("Starting ALG App Store");
+    Logger::info(QString("Qt version: %1").arg(qVersion()));
+    
+    MainWindow window;
+    window.show();
+    
+    Logger::info("Application window shown");
+    
+    int result = app.exec();
+    
+    Logger::info("Application exiting");
+    return result;
+}
