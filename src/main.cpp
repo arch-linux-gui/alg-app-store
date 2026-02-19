c79a2d77833f6154e7a96ac857855783bcd8aa4b
demonkillerr <gaurangvishwakarma619@gmail.com>
2026-02-19 19:38:49 +0000

    mirgrate to spdlog & ignore logs

--- src/main.cpp
+++ src/main.cpp
@@ -3,27 +3,32 @@
 #include <QApplication>
 #include <QStyleFactory>
 
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    
+int
+main( int argc, char* argv[] )
+{
+    QApplication app( argc, argv );
+
     // Set application metadata
-    app.setApplicationName("ALG App Store");
-    app.setApplicationVersion("2.0.0");
-    app.setOrganizationName("Arch Linux GUI");
-    
+    app.setApplicationName( "ALG App Store" );
+    app.setApplicationVersion( "2.0.0" );
+    app.setOrganizationName( "Arch Linux GUI" );
+
     // Set application style
-    app.setStyle(QStyleFactory::create("Fusion"));
-    
-    Logger::info("Starting ALG App Store");
-    Logger::info(QString("Qt version: %1").arg(qVersion()));
-    
+    app.setStyle( QStyleFactory::create( "Fusion" ) );
+
+    // Initialize Logger
+    Logger::init();
+
+    Logger::info( "Starting ALG App Store" );
+    Logger::info( QString( "Qt version: %1" ).arg( qVersion() ) );
+
     MainWindow window;
     window.show();
-    
-    Logger::info("Application window shown");
-    
+
+    Logger::info( "Application window shown" );
+
     int result = app.exec();
-    
-    Logger::info("Application exiting");
+
+    Logger::info( "Application exiting" );
     return result;
 }
--- src/utils/logger.h
+++ src/utils/logger.h
@@ -1,25 +1,55 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
-#include <QDebug>
 #include <QString>
+#include <memory>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/spdlog.h>
+#include <vector>
 
-namespace Logger {
-    inline void info(const QString &message) {
-        qDebug() << "[INFO]" << message;
-    }
-    
-    inline void warning(const QString &message) {
-        qWarning() << "[WARNING]" << message;
-    }
-    
-    inline void error(const QString &message) {
-        qCritical() << "[ERROR]" << message;
-    }
-    
-    inline void debug(const QString &message) {
-        qDebug() << "[DEBUG]" << message;
-    }
+namespace Logger
+{
+inline void
+init()
+{
+    auto console_sink = std::make_shared< spdlog::sinks::stdout_color_sink_mt >();
+    console_sink->set_level( spdlog::level::debug );
+
+    auto file_sink = std::make_shared< spdlog::sinks::basic_file_sink_mt >( "logs/app.log", true );
+    file_sink->set_level( spdlog::level::debug );
+
+    std::vector< spdlog::sink_ptr > sinks { console_sink, file_sink };
+    auto logger = std::make_shared< spdlog::logger >( "multi_sink", sinks.begin(), sinks.end() );
+
+    spdlog::set_default_logger( logger );
+    spdlog::set_level( spdlog::level::debug );
+    spdlog::flush_on( spdlog::level::debug );
 }
 
-#endif // LOGGER_H
+inline void
+info( const QString& message )
+{
+    spdlog::info( message.toStdString() );
+}
+
+inline void
+warning( const QString& message )
+{
+    spdlog::warn( message.toStdString() );
+}
+
+inline void
+error( const QString& message )
+{
+    spdlog::error( message.toStdString() );
+}
+
+inline void
+debug( const QString& message )
+{
+    spdlog::debug( message.toStdString() );
+}
+}  // namespace Logger
+
+#endif  // LOGGER_H
